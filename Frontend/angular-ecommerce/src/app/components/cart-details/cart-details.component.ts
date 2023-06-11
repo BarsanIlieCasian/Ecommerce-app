7720881c3e258657d1e913eb8625b06ed04b91a2
Barsan Ilie Casian <iliutabarsan@gmail.com>
2023-06-11 20:28:05 +0300

    cart-details

---
+++ Frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {CartItem} from "../../common/cart-item";
+import {CartService} from "../../services/cart.service";
+import {dateComparator} from "@ng-bootstrap/ng-bootstrap/datepicker/datepicker-tools";
+
+@Component({
+  selector: 'app-cart-details',
+  templateUrl: './cart-details.component.html',
+  styleUrls: ['./cart-details.component.css']
+})
+export class CartDetailsComponent implements OnInit{
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) {
+  }
+
+  ngOnInit() {
+    this.listCartDetails();
+  }
+
+  listCartDetails(){
+    this.cartItems = this.cartService.cartItems;
+    this.cartService.totalPrice.subscribe(data =>{this.totalPrice = data} );
+    this.cartService.totalQuantity.subscribe(data => {this.totalQuantity = data} );
+  }
+
+  addToCart(cartItem: CartItem){
+    this.cartService.addToCart(cartItem);
+  }
+
+  subtractFromCart(cartItem: CartItem){
+    this.cartService.subtractFromCart(cartItem);
+  }
+}
